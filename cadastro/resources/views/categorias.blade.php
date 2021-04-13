7da7aedf3c5f8aa47f8d064b8421b22f10930aeb
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-04-12 22:43:43 -0300

    Update categorias.blade.php

--- cadastro/resources/views/categorias.blade.php
+++ cadastro/resources/views/categorias.blade.php
@@ -5,7 +5,7 @@
  <h5 class="card-title">Cadastro de Categorias</h5>
 
 @if(count($cats) > 0)
-    <table class="table table-ordered table-hover">
+    <table style="border: 1px solid #dee2e6" class="table table-bordered table-ordered table-hover table-striped">
         <thead>
             <tr>
                 <th>Código</th>
@@ -21,7 +21,7 @@
     <td>{{$cat->nome}}</td>
     <td>
     <a href="{{ route('categorias.edit', $cat['id']) }}" class="btn btn-sm btn-primary">Editar</a>
-    <form action="{{ route('categorias.destroy', $cat['id']) }}" method="POST">
+    <form style="margin: 0; align-self: center;" action="{{ route('categorias.destroy', $cat['id']) }}" method="POST">
         @csrf
         @method('DELETE')
         <input type="submit" class="btn btn-sm btn-danger" value="Apagar">
@@ -31,11 +31,8 @@
  @endforeach
  </tbody>
  </table>
-
+ @endif
  <div class="card-footer">
     <a href="/categorias/create" class="btn btn-sm btn-primary" role="button">Nova Categoria</a>
  </div>
-
-@endif
-
 @endsection
